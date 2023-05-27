7983267c59b81f206401392727867bc913b9fb48
marimotona <ssmmo.06021224@gmail.com>
2023-05-27 13:23:02 +0900

    calcurate_program

--- day3/lecture/calcurate.py
+++ day3/lecture/calcurate.py
@@ -1,32 +1,41 @@
 while True:
     line = input()
-    answer = 0
+    answer = 0.0
     index = 0
+    ope = 1
     while index < len(line):
-       ope = 1
-       flag_point = 0
-       flag_float = 0
-       number = 0
-       if line[index].isdigit() or line[index] == '.':
-           if line[index] == '.':
-              flag_point == 1
-           while index < len(line) and line[index].isdigit():
-               number = number * 10 + int(line[index])
-               index += 1
-               if ope == 1:
-                answer += number 
-               elif ope == 2: #最初の数字がここに入ってしまうから
-                answer -= number           
-       elif line[index] == '+':
-           index += 1
-           ope += 1
-       elif line[index] == '-':
-           index += 1
-           ope += 1
-       else:
-           print('Invalid character found: ' + line[index])
-           exit(1)
-    print("answer = %d\n" % answer)
+        number = 0.0
+        while index < len(line) and (line[index].isdigit() or line[index] == '.'):
+            if line[index] == '.':
+                index += 1
+                decimal_place = 1
+                while index < len(line) and line[index].isdigit():
+                    number = number + int(line[index]) / (10 ** decimal_place)
+                    print("number = %f\n" % number)
+                    print("answer = %f\n" % answer)
+                    decimal_place += 1
+                    index += 1
+                break
+            else:
+                number = number * 10 + int(line[index])
+                print("number = %f\n" % number)
+                index += 1
+        if ope == 1:
+            answer += number 
+        elif ope == 2: #最初の数字がここに入ってしまうから
+            answer -= number  
+
+        if index < len(line):         
+            if line[index] == '+':
+                index += 1
+                ope = 1
+            elif line[index] == '-':
+                index += 1
+                ope = 2
+            else:
+                print('Invalid character found: ' + line[index])
+                exit(1)
+    print("answer = %f\n" % answer)
 
 # 直前の演算子をみるようにする、indexの一個前をみる
 # モジュール化、関数の責任範囲を明確にする
