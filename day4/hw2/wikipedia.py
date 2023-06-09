8213e9acb3499ce9d84f14da7598a3fc94bb1a16
marimotona <ssmmo.06021224@gmail.com>
2023-06-09 15:30:19 +0900

    create file

---
+++ day4/hw2/wikipedia.py
@@ -0,0 +1,177 @@
+import sys
+import collections
+import heapq
+
+class Wikipedia:
+
+    # Initialize the graph of pages.
+    def __init__(self, pages_file, links_file):
+
+        # A mapping from a page ID (integer) to the page title.
+        # For example, self.titles[1234] returns the title of the page whose
+        # ID is 1234.
+        self.titles = {}
+
+        # A set of page links.
+        # For example, self.links[1234] returns an array of page IDs linked
+        # from the page whose ID is 1234.
+        self.links = {}
+
+        # Read the pages file into self.titles.
+        with open(pages_file, encoding='utf-8') as file:
+            for line in file:
+                (id, title) = line.rstrip().split(" ")
+                id = int(id)
+                assert not id in self.titles, id
+                self.titles[id] = title
+                self.links[id] = []
+        print("Finished reading %s" % pages_file)
+
+        # Read the links file into self.links.
+        with open(links_file, encoding='utf-8') as file:
+            for line in file:
+                (src, dst) = line.rstrip().split(" ")
+                (src, dst) = (int(src), int(dst))
+                assert src in self.titles, src
+                assert dst in self.titles, dst
+                self.links[src].append(dst)
+        print("Finished reading %s" % links_file)
+        print()
+
+
+    # Find the longest titles. This is not related to a graph algorithm at all
+    # though :)
+    def find_longest_titles(self):
+        titles = sorted(self.titles.values(), key=len, reverse=True)
+        print("The longest titles are:")
+        count = 0
+        index = 0
+        while count < 15 and index < len(titles):
+            if titles[index].find("_") == -1:
+                print(titles[index])
+                count += 1
+            index += 1
+        print()
+
+
+    # Find the most linked pages.
+    def find_most_linked_pages(self):
+        link_count = {}
+        for id in self.titles.keys():
+            link_count[id] = 0
+
+        for id in self.titles.keys():
+            for dst in self.links[id]:
+                link_count[dst] += 1
+
+        print("The most linked pages are:")
+        link_count_max = max(link_count.values())
+        for dst in link_count.keys():
+            if link_count[dst] == link_count_max:
+                print(self.titles[dst], link_count_max)
+        print()
+
+
+    # Find the shortest path
+    # |start|: The title of the start page.
+    # |goal|: The title of the goal page.
+    def find_shortest_path(self, start, goal):
+        #------------------------#
+        # Write your code here!  #
+        #------------------------#
+        start_id = None
+        goal_id = None
+        for id, title in self.titles.items():
+            if title == start:
+                start_id = id
+            if title == goal:
+                goal_id = id
+
+        if start_id is None or goal_id is None:
+            print("Not Found")
+            return None
+
+        visited = set()
+        queue = collections.deque([(start_id, [])])
+
+        while queue:
+            (curr_node, path) = queue.popleft()
+            if curr_node not in visited:
+                visited.add(curr_node)
+                path = path + [curr_node]
+
+                if curr_node == goal_id:
+                    shortest_path =  [self.titles[i] for i in path]
+                    print('shortest path :')
+                    for page in shortest_path:
+                        print(page)
+                    return shortest_path
+                
+                for next_node in self.links[curr_node]:
+                    if next_node not in visited:
+                        queue.append((next_node, path))
+        pass
+
+
+
+    # Calculate the page ranks and print the most popular pages.
+    def find_most_popular_pages(self):
+        #------------------------#
+        # Write your code here!  #
+        #------------------------#
+        popular = {id: 1.0 for id in self.titles.keys()}
+        share_ratio = 0.85
+        length = len(self.titles)
+        for _ in range(100):
+            # for id, score in popular.items():
+            #     print(f'ID: {id}, popular: {score}')
+            new_popular = {id: (1.0 - share_ratio) / length for id in self.titles.keys()}
+            # for id in self.titles.keys():
+            #     if self.links[id]:
+            #         distribute_popular = share_ratio * popular[id] / len(self.links[id])
+            #         for linked in self.links[id]:
+            #             new_popular[linked] += distribute_popular
+            #     else:
+            #         distribute_popular = popular[id] / length
+            #         for linked in new_popular.keys():
+            #             new_popular[linked] += distribute_popular
+            for id, score in popular.items():
+                if self.links[id]:
+                    distribute = share_ratio * score / len(self.links[id])
+                    for node in self.links[id]:
+                        new_popular[node] += distribute
+                else:
+                    distribute = score / length
+                    for node in self.titles.keys():
+                        new_popular[node] += distribute
+
+            popular = new_popular
+        
+        # popular_score = sorted(popular.items(), reverse=True)
+        # for i in range(10):
+        #     print(f'ID: {popular_score[i][0]}, popular: {popular_score[i][1]}')
+
+        top_pages = heapq.nlargest(10, popular.items(), key=lambda x: x[1])
+        for id, score in top_pages:
+            print(f'ID: {id}, popular: {score}')
+        pass
+
+
+    # Do something more interesting!!
+    def find_something_more_interesting(self):
+        #------------------------#
+        # Write your code here!  #
+        #------------------------#
+        pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage: %s pages_file links_file" % sys.argv[0])
+        exit(1)
+
+    wikipedia = Wikipedia(sys.argv[1], sys.argv[2])
+    # wikipedia.find_longest_titles()
+    # wikipedia.find_most_linked_pages()
+    # wikipedia.find_shortest_path("渋谷", "小野妹子")
+    wikipedia.find_most_popular_pages()
